2a44f0fea1f7ddd8e1976ebfc53e9a9500bf3262
Rameez khan <sardarrameez5555@gmail.com>
2020-09-29 15:46:43 +0500

    Write test for App.js

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
+
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test("Render test", ()=>{
+  const {getByText}=render(<App></App>);
+  const linkElement=getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
-});
+})
+
+test("Find word",()=>{
+  const response=render(<App/>);
+  const word=response.getByText(/World/);
+  expect(word).toBeInTheDocument();
+})
+
+test("Find placeholder",()=>{
+  const response=render(<App/>);
+  const word=response.getByPlaceholderText("Enter your name");
+  expect(word).toBeInTheDocument();
+})
